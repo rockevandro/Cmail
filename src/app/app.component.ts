09b442c6a30c14aafcbcb6aa0bff3b640ab34e19
caelum <caelum@caelum133-03>
2019-06-22 16:30:30 -0300

    Inicio projeto: - criação do projeto - css e header component - menu mobile - form envio de email - envia email simples - validação e variáveis de template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmail';
+  private _isNewEmailFormOpen = false;
+  emailList = [];
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  }
+
+  get isNewEmailFormOpen(){
+    return this._isNewEmailFormOpen;
+  }
+
+  toggleNewEmailForm(){
+    this._isNewEmailFormOpen = !this._isNewEmailFormOpen;
+  }
+
+  handleNewEmail(formEmail: NgForm){
+
+    if (formEmail.invalid)
+      return;
+
+    this.emailList.push(this.email);
+
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    };
+
+    formEmail.reset();
+  }
 }

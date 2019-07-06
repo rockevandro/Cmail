ff536ad57a0f45b454eb7d751e3087ebbf605d6c
caelum <caelum@caelum133-03>
2019-07-06 14:19:06 -0300

    Enviando dados do formulário com HttpClient

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient, HttpResponseBase } from '@angular/common/http';
+import { HttpClient, HttpResponseBase, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'cadastro',
@@ -9,6 +11,8 @@ import { map, catchError } from 'rxjs/operators';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+
+  mensagensErro: any;
 
   formCadastro = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -18,12 +22,25 @@ export class CadastroComponent implements OnInit {
     Validators.pattern('[0-9]{4}-?[0-9]{4}[0-9]?')]),
     avatar: new FormControl('', [Validators.required], this.validaImagem.bind(this))
   });
-  
+
 
   handleCadastrarUsuario() {
+
     if (this.formCadastro.valid) {
-      console.log(this.formCadastro.value);
-      this.formCadastro.reset();
+      const userData = new User(this.formCadastro.value);
+
+      this.httpClient.post('http://localhost:3200/users', userData)
+        .subscribe(() => {
+          console.log('Cadastrado com sucesso');
+          this.formCadastro.reset();
+
+          setTimeout(() => {
+            this.roteador.navigate(['']);
+          }, 1000);
+        },
+          (responseError: HttpErrorResponse) => {
+            this.mensagensErro = responseError.error.body;
+          })
     }
     else
       this.validarTodosOsCamposDoFormulario(this.formCadastro);
@@ -36,22 +53,22 @@ export class CadastroComponent implements OnInit {
     })
   }
 
-  validaImagem(campoDoFormulario: FormControl){
+  validaImagem(campoDoFormulario: FormControl) {
     return this.httpClient.head(campoDoFormulario.value, {
       observe: 'response'
     })
-    .pipe(
-      map((response: HttpResponseBase)=> {
-        return response.ok ? null : { urlInvalida: true }
-      }),
-      catchError((error) => {
-        return [{ urlInvalida: true}]
-      })
-    
-    )
+      .pipe(
+        map((response: HttpResponseBase) => {
+          return response.ok ? null : { urlInvalida: true }
+        }),
+        catchError((error) => {
+          return [{ urlInvalida: true }]
+        })
+
+      )
   }
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private roteador: Router) { }
 
   ngOnInit() {
   }
